2c3f71014174ad596cac1525bfbe9e8e027535b3
balor <me@balor.pl>
2025-03-05 22:27:54 +0100

    removed flatpaks..

--- build.sh
+++ build.sh
@@ -14,21 +14,6 @@ systemctl enable keyd.service
 # btop
 dnf5 -y install btop
 
-flatpak remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# flatpaks - comms
-flatpak --system install -y flathub org.signal.Signal
-flatpak --system install -y flathub com.mattermost.Desktop
-flatpak --system install -y flathub com.slack.Slack
-
-# flatpacks - applications
-flatpak --system install -y flathub org.kde.ktorrent
-flatpak --system install -y flathub org.wezfurlong.wezterm
-flatpak --system install -y flathub md.obsidian.Obsidian
-flatpak --system install -y flathub app.zen_browser.zen
-flatpak --system install -y flathub com.github.wwmm.easyeffects
-flatpak --system install -y flathub com.spotify.Client
-
 ### Install system flatpaks
 
 # Use a COPR Example:
